50b9fddae62a618e0acc9c6f8e291311a43d76c1
lima <igorrmatheus2005@gmail.com>
2026-03-18 21:50:32 -0300

    Agenda Pokémon
    Dia 18/03/26

---
+++ pokemon_agenda/pokemon_agenda/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace pokemon_agenda
+{
+    partial class fnCadastroPokemon
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fnCadastroPokemon));
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtLimpar = new System.Windows.Forms.Button();
+            this.txtSalvar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numNivel = new System.Windows.Forms.NumericUpDown();
+            this.cbTipo = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numNivel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(95, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Agenda de Pokémon";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbTipo);
+            this.groupBox1.Controls.Add(this.numNivel);
+            this.groupBox1.Controls.Add(this.txtLimpar);
+            this.groupBox1.Controls.Add(this.txtSalvar);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Location = new System.Drawing.Point(26, 130);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(309, 200);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastro Pokémon";
+            // 
+            // txtLimpar
+            // 
+            this.txtLimpar.Location = new System.Drawing.Point(157, 162);
+            this.txtLimpar.Name = "txtLimpar";
+            this.txtLimpar.Size = new System.Drawing.Size(145, 32);
+            this.txtLimpar.TabIndex = 7;
+            this.txtLimpar.Text = "Limpar";
+            this.txtLimpar.UseVisualStyleBackColor = true;
+            this.txtLimpar.Click += new System.EventHandler(this.txtLimpar_Click);
+            // 
+            // txtSalvar
+            // 
+            this.txtSalvar.Location = new System.Drawing.Point(6, 162);
+            this.txtSalvar.Name = "txtSalvar";
+            this.txtSalvar.Size = new System.Drawing.Size(145, 32);
+            this.txtSalvar.TabIndex = 6;
+            this.txtSalvar.Text = "Salvar";
+            this.txtSalvar.UseVisualStyleBackColor = true;
+            this.txtSalvar.Click += new System.EventHandler(this.txtSalvar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 26);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Nivel:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tipo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(107, 19);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(187, 31);
+            this.txtNome.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::pokemon_agenda.Properties.Resources.pokeball;
+            this.pictureBox1.Image = global::pokemon_agenda.Properties.Resources.pokeball1;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numNivel
+            // 
+            this.numNivel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numNivel.Location = new System.Drawing.Point(107, 98);
+            this.numNivel.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numNivel.Name = "numNivel";
+            this.numNivel.Size = new System.Drawing.Size(187, 32);
+            this.numNivel.TabIndex = 8;
+            this.numNivel.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // cbTipo
+            // 
+            this.cbTipo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTipo.FormattingEnabled = true;
+            this.cbTipo.Items.AddRange(new object[] {
+            "Normal",
+            "",
+            "Fogo",
+            "",
+            "Água",
+            "",
+            "Grama",
+            "",
+            "Elétrico",
+            "",
+            "Gelo",
+            "",
+            "Lutador",
+            "",
+            "Venenoso",
+            "",
+            "Terra",
+            "",
+            "Voador",
+            "",
+            "Psíquico",
+            "",
+            "Inseto",
+            "",
+            "Pedra",
+            "",
+            "Fantasma",
+            "",
+            "Dragão"});
+            this.cbTipo.Location = new System.Drawing.Point(107, 58);
+            this.cbTipo.Name = "cbTipo";
+            this.cbTipo.Size = new System.Drawing.Size(187, 33);
+            this.cbTipo.TabIndex = 9;
+            // 
+            // fnCadastroPokemon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fnCadastroPokemon";
+            this.Text = "Pokémon Agenda ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numNivel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button txtLimpar;
+        private System.Windows.Forms.Button txtSalvar;
+        private System.Windows.Forms.NumericUpDown numNivel;
+        private System.Windows.Forms.ComboBox cbTipo;
+    }
+}
+
---
+++ pokemon_agenda/pokemon_agenda/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pokemon_agenda
+{
+    public partial class fnCadastroPokemon : Form
+    {
+        public fnCadastroPokemon()
+        {
+            InitializeComponent();
+        }
+
+        private void fnLimpar()
+        {  
+            txtNome.Clear();
+            cbTipo.SelectedIndex = -1;
+            numNivel.Value = 1;
+
+            txtNome.Focus();
+
+
+        }
+        private void txtSalvar_Click(object sender, EventArgs e)
+        {
+            string nomePokemon = txtNome.Text;  //textBox    
+            string tipoPokemon = cbTipo.Text;   //comboBox
+            int nivelPokemon = (int)numNivel.Value; //numericUpDown
+
+
+            MessageBox.Show($"O pokémon {nomePokemon} do tipo {tipoPokemon} " +
+                $"foi cadastrado com o nivel {nivelPokemon}");
+
+            fnLimpar();
+        }
+
+
+        private void txtLimpar_Click(object sender, EventArgs e)
+        {
+            fnLimpar();
+        }
+    }
+}
